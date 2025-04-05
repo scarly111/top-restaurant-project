647fcae048575826112ad783a899940f8274892a
scarly111 <yigitcagri48@gmail.com>
2025-04-05 11:36:11 +0300

    The other pages added to index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import loadHome from './home';
+import loadMenu from './menu';
+import loadContact from './contact';
 
 function clearContent() {
-  document.getElementById('content').textContent = '';
+    document.getElementById('content').textContent = '';
 }
 
 document.getElementById('homeBtn').addEventListener('click', () => {
-  clearContent();
-  loadHome();
+    clearContent();
+    loadHome();
+});
+
+document.getElementById('menuBtn').addEventListener('click', () => {
+    clearContent();
+    loadMenu();
+});
+
+document.getElementById('contactBtn').addEventListener('click', () => {
+    clearContent();
+    loadContact();
 });
 
 // Load homepage by default
